7a55a0728798f490b5d2e2f893fb8933d64bd012
votienthuan97 <votienthuan97@gmail.com>
2026-08-02 00:55:53 +0700

    Restore customer selector placeholder

--- app/components/RevenueDashboard.tsx
+++ app/components/RevenueDashboard.tsx
@@ -1835,6 +1835,7 @@ export function RevenueDashboard({ customerMode = false }: RevenueDashboardProps
                   <option key={customer} value={customer}>{customer}</option>
                 ))}
               </MonthSelect>
+              {!selectedCustomer && <CustomerSelectPlaceholder>Chọn Khách Hàng</CustomerSelectPlaceholder>}
               <DropdownIcon>
                 <ChevronDown size={18} />
               </DropdownIcon>
